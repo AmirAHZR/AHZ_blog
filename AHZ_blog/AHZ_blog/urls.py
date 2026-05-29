67e51b34b6eb1430b6eec94de1838d0d31ca5678
AmirAHZR <amirhosseinzareiifar@gmail.com>
2026-05-29 13:55:28 +0330

    Initial commation

--- AHZ_blog/AHZ_blog/urls.py
+++ AHZ_blog/AHZ_blog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for amir_blog project.
+URL configuration for AHZ_blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
